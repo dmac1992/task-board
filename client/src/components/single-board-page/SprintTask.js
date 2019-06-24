aaacbd1eb5c5202d74ac1e5953bb4b25db3528b3
Daniel McCarthy <dmccarthy2012@hotmail.co.uk>
2019-06-24 20:54:35 +1000

    updated sprint container task cards, gave it basic structure

--- client/src/components/single-board-page/SprintTask.js
+++ client/src/components/single-board-page/SprintTask.js
@@ -4,28 +4,104 @@ import { connect } from 'react-redux';
 
 import  { setActiveModal } from 'actions/modal';
 import TaskPopup from 'components/single-board-task-popup/TaskPopup';
+import { NONAME } from 'dns';
 
 
 const Container = styled.div`
     width: 100%;
-    height: 70px;
-    background-color: red;
+    background-color: white;
     margin-bottom: 5px;
+    background-color: 0 1px 0 rgba(9,30,66,.25);
+    border-radius: 3px;
+    cursor: pointer;
+    padding: 5px;
 `;
+
+const TopHalf = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 30px;
+`;
+
+const Title = styled.span``;
+const EditIcon = styled.span`
+`;
+
+
+const BottomHalf = styled.div`
+    display: flex;
+    align-items: center;
+    height: 30px;
+`;
+const WatchingIcon = styled.span`
+    margin-right: 8px;
+    font-size: 12px;
+`;
+const ChecklistIcon = styled.span`
+    margin-right: 2px;
+    font-size: 12px;
+`;
+const ChecklistFraction = styled.span`
+    font-size: 12px;
+`;
+
+const UserIconContainer = styled.div`
+    margin-left: auto;
+`;
+const UserIcon = styled.span`
+    border-radius: 50%;
+    background-color: #dfe1e6;
+    display: block;
+    width: 28px;
+    height: 28px;
+    text-align: center;
+    line-height: 28px;
+`;
+
+
 
 class SprintTask extends Component {
 
     state = {
-        'addingCard' : false
+        hovered: false
     }
 
     createTaskModal = () => {
         this.props.setActiveModal(TaskPopup);
     }
 
+    onMouseEnter = () => {
+        this.setState({hovered: true});
+    }
+
+    onMouseLeave = () => {
+        this.setState({hovered: false});
+    }
+
+    hoverContainerStyles = {
+        backgroundColor: '#f4f5f7'
+    }
+
+    hideElement = {
+        display: 'none'
+    }
+
     render() {
         return (
-            <Container onClick={this.createTaskModal}>
+            <Container style={this.state.hovered ? this.hoverContainerStyles : null } onClick={this.createTaskModal} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
+                <TopHalf>
+                    <Title>task title</Title>
+                    <EditIcon className='icon-pencil' style={!this.state.hovered ? this.hideElement : null}  />
+                </TopHalf>
+                <BottomHalf>
+                    <WatchingIcon className='icon-eye' />
+                    <ChecklistIcon className='icon-check-square-o' />
+                    <ChecklistFraction>2/5</ChecklistFraction>
+                    <UserIconContainer>
+                        <UserIcon>M</UserIcon>
+                    </UserIconContainer>
+                </BottomHalf>
             </Container>
         )
     }
